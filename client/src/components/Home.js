ca1c62d9c45117aa674cb7af59327f8bc41c1821
Spencer Richards <spencer.richards7@gmail.com>
2018-06-22 14:40:43 -0600

    start update home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import { Header } from 'semantic-ui-react';
 
-class Home extends Component {
+class Home extends React.Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <HomeContainer>
+        <Circle />
+        {/* <Header as='h1' textAlign='center'>Home Component</Header> */}
+      </HomeContainer>
     );
   }
 }
+
+const HomeContainer = styled.div`
+  height: 800px;
+  border: 2px solid red;
+`;
 
 export default Home;
